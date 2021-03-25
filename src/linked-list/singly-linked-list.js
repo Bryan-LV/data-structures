7512b19c00923c02268e88675e25772af824fd66
Bryan Almaraz <balmaraz@apartments247.com>
2021-03-25 08:31:03 -0600

    init commit

---
+++ src/linked-list/singly-linked-list.js
@@ -0,0 +1,32 @@
+class Node {
+  constructor(data, pointer) {
+    this.data = data;
+    this.pointer = pointer;
+  }
+}
+
+class SinglyLinkedList {
+  constructor(head) {
+    this.head = head;
+    this.tail = null;
+  }
+
+  // ADD OPERATIONS
+  // add to beginning
+  // add to end
+  // add to 'X'
+
+  // DELETE OPERATIONS
+  // delete from end
+  // delete from beginning
+  // delete from 'X'
+
+  // Find Operations
+  // search for single match
+  // search for all matches
+
+  // Useful methods
+  // length
+  // get head
+  // get tail
+}
